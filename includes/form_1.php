810cec61a75a4dff0cacf21dd50e638aeec94811
HERAUX-BOCQUET Jean-Claude <72662725+Lapitos@users.noreply.github.com>
2020-12-19 16:33:25 +0100

    Version 0.3
    - Ajout de manifest.json
    - Ajout de robots.txt
    - Ajout de sitemap.xml

--- includes/form_1.php
+++ includes/form_1.php
@@ -1,23 +1,23 @@
 <?php	
-	if(empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['input_504']) && strlen($_POST['input_504']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0)
+	if(empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['phone']) && strlen($_POST['phone']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0)
 	{
 		return false;
 	}
 	
 	$name = $_POST['name'];
 	$email = $_POST['email'];
-	$input_504 = $_POST['input_504'];
+	$phone = $_POST['phone'];
 	$message = $_POST['message'];
 	
-	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
+	$to = 'contact@hb-creative.net'; // Email submissions are sent to this email
 
 	// Create email	
-	$email_subject = "Message from your website";
-	$email_body = "You have received a new message. \n\n".
-				  "Name: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
+	$email_subject = "Message reçu";
+	$email_body = "Vous avez un nouveau message. \n\n".
+				  "Nom: $name \nEmail: $email \nTéléphone: $phone \nMessage: $message \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
-	$headers .= "From: contact@yoursite.com\n";
-	$headers .= "Reply-To: $input_504";	
+	$headers .= "From: contact@hb-creative.net\n";
+	$headers .= "Reply-To: $email";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
 	return true;			
